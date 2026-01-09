05ede777fe159e3a1630bcafb2ab4c3ed4e4050f
Laziz <lazizakbarhonov7@gmail.com>
2026-01-09 20:15:55 +0300

    Main.tsx config

--- src/Layouts/RegisterLayouts.tsx
+++ src/Layouts/RegisterLayouts.tsx
@@ -1,17 +1,1 @@
-import AuthImg from "../assets/img/photos/AuthImg.png";
 
-interface RegisterProps {
-  children: React.ReactNode
-}
-
-export default function RegisterLayouts() {
-  return (
-    <>
-      <div className="">
-        <div className="">
-          <img src={AuthImg} alt="" />
-        </div>
-      </div>
-    </>
-  )
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+
+const client = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <QueryClientProvider client={client}>
+     <RouterProvider router={router}/>
+  </QueryClientProvider>
 )
