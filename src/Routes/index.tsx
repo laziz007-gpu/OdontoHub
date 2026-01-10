edd06038098a989435f06552dc6b2f2d955d31f6
Laziz <lazizakbarhonov7@gmail.com>
2026-01-10 14:44:30 +0300

    ...

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import { paths } from "./path";
 import Welcome from "../Pages/Welcome"
+import PublickRoute from "../HOC/PublickRoute";
 
 export const router = createBrowserRouter([
     {
         path: paths.welcome,
-        element: <Welcome/>
+        element: <PublickRoute/>,
+        children: [{
+            index: true,
+            element: <Welcome/>
+        }]
     }
 ])
--- src/Routes/path.ts
+++ src/Routes/path.ts
@@ -1,3 +1,3 @@
 export const paths = {
-    welcome: '/'
+    welcome: '/Welcome'
 } as const
