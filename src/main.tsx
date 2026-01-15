091aeecce1b2e3310ebb63c547692cfbeeeea170
laziz007-gpu <lazizakbarhonov7@gmail.com>
2026-01-15 22:58:29 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,12 @@
-import { StrictMode } from 'react'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Menu from './pages/Menu'
-import './index.css'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Menu />,
-  },
-])
+
+const client = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+  <QueryClientProvider client={client}>
+     <RouterProvider router={router}/>
+  </QueryClientProvider>
 )
